be5e19b0ad1ef3ce65abe333c65ae835928e00bd
Aaron Franke <arnfranke@yahoo.com>
2020-06-14 20:50:27 -0400

    Implement some in-game debug text

---
+++ level/debug.gd
@@ -0,0 +1,9 @@
+extends Label
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_debug"):
+		visible = !visible
+	
+	text = "FPS: " + str(Engine.get_frames_per_second())
+	text += "\nVSync: " + ("on" if ProjectSettings.get_setting("display/window/vsync/use_vsync") else "off")
+	text += "\nMemory: " + "%3.2f" % (OS.get_static_memory_usage() / 1048576.0) + " MiB"
