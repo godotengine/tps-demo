83950371f53843f2b42c578a883767272d94a490
Emre <53046302+emrecrpnl@users.noreply.github.com>
2024-08-29 15:16:28 +0300

    Added functional doors to the game map with animations and collisions
    Integrated fully functional doors into the game map, utilizing existing animations and collision objects. This update ensures that all doors within the game are now interactive and operate correctly, providing a more immersive experience. Each door has been configured to open and close as intended when interacted with by the player.

--- door/door.gd
+++ door/door.gd
@@ -1,10 +1,34 @@
 extends Area3D
 
-var open = false
+@export var open = false
 
-@onready var animation_player = $DoorModel/AnimationPlayer
+@onready var animation_player :AnimationPlayer= $DoorModel2/AnimationPlayer
+
+@onready var left_collision = $"DoorModel2/armature-doorsimple/Skeleton3D/doorleft/CollisionShape3D"
+@onready var right_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorright/CollisionShape3D"
+@onready var upper_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorupper/CollisionShape3D"
+@onready var lower_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorlower/CollisionShape3D"
+
 
 func _on_door_body_entered(body):
 	if not open and body is Player:
 		animation_player.play("doorsimple_opening")
 		open = true
+
+func _process(delta):
+	if float(animation_player.current_animation_position) < 1.9:
+		print_debug("door closed")
+		left_collision.disabled = false
+		right_collision.disabled = false
+		upper_collision.disabled = false
+		lower_collision.disabled = false
+	else:
+		left_collision.disabled = true
+		right_collision.disabled = true
+		upper_collision.disabled = true
+		lower_collision.disabled = true
+
+func _on_body_exited(body):
+	if open and body is Player:
+		animation_player.play_backwards("doorsimple_opening")
+		open = false
