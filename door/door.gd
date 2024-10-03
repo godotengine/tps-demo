e8f87807578e14ef5c484c0cb5f031f1fe6b9edc
Aaron Franke <arnfranke@yahoo.com>
2024-10-03 14:21:28 -0700

    Fix GDScript style

--- door/door.gd
+++ door/door.gd
@@ -1,29 +1,28 @@
 extends Area3D
 
-@export var open = false
-@export var is_locked = false
 
-@onready var animation_player :AnimationPlayer= $DoorModel2/AnimationPlayer
+@export var open: bool = false
+@export var is_locked: bool = false
 
-@onready var left_collision = $"DoorModel2/armature-doorsimple/Skeleton3D/doorleft/CollisionShape3D"
-@onready var right_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorright/CollisionShape3D"
-@onready var upper_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorupper/CollisionShape3D"
-@onready var lower_collision =$"DoorModel2/armature-doorsimple/Skeleton3D/doorlower/CollisionShape3D"
-@onready var lock_indicator_mesh = $"DoorModel2/armature-doorsimple/Skeleton3D/doorsimple"
+@onready var animation_player: AnimationPlayer= $DoorModel2/AnimationPlayer
 
-var override_material : StandardMaterial3D = StandardMaterial3D.new()
+@onready var _door_skeleton: Skeleton3D = $"DoorModel2/armature-doorsimple/Skeleton3D"
+@onready var left_collision: CollisionShape3D = _door_skeleton.get_node(^"doorleft/CollisionShape3D")
+@onready var right_collision: CollisionShape3D = _door_skeleton.get_node(^"doorright/CollisionShape3D")
+@onready var upper_collision: CollisionShape3D = _door_skeleton.get_node(^"doorupper/CollisionShape3D")
+@onready var lower_collision: CollisionShape3D = _door_skeleton.get_node(^"doorlower/CollisionShape3D")
+@onready var lock_indicator_mesh: MeshInstance3D = _door_skeleton.get_node(^"doorsimple")
+
+var override_material := StandardMaterial3D.new()
+
 
 func _ready():
 	lock_indicator_mesh.set_surface_override_material(1, override_material)
 	if is_locked:
-		override_material.albedo_color = Color(1,0,0)
+		override_material.albedo_color = Color.RED
 	elif not is_locked:
-		override_material.albedo_color = Color(0,1,0)
+		override_material.albedo_color = Color.GREEN
 
-func _on_door_body_entered(body):
-	if not open and body is Player and not is_locked:
-		animation_player.play("doorsimple_opening")
-		open = true
 
 func _process(_delta):
 	if float(animation_player.current_animation_position) < 1.9:
@@ -36,6 +35,13 @@ func _process(_delta):
 		right_collision.disabled = true
 		upper_collision.disabled = true
 		lower_collision.disabled = true
+
+
+func _on_door_body_entered(body):
+	if not open and body is Player and not is_locked:
+		animation_player.play("doorsimple_opening")
+		open = true
+
 
 func _on_body_exited(body):
 	if open and body is Player and not is_locked:
